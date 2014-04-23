a09a6d7b566561ce7943ba99af3d3fe279f88e85
Cristiano Fontes <cristianofontes@gmail.com>
2014-04-23 16:57:04 +0800

    fixing packages

---
+++ test/ojleet/tests/AtlTests.java
@@ -0,0 +1,29 @@
+package ojleet.tests;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class AtlTests {
+
+	/**
+	 * @param args
+	 * @author Cristiano
+	 */
+	public static void main(String[] args) {
+		List<Integer> ints = new ArrayList<Integer>();
+		Collections.reverse(ints);
+		Collections.unmodifiableList(ints);
+	}
+
+	public int[] twoSum(int[] numbers, int target) {
+        for(int x = 1; x < numbers.length; x++){
+        	for(int y = x+1; y < numbers.length; y++){
+        		if(numbers[x] + numbers[y] == target){
+					return new int[] { x, y };
+        		}
+        	}
+        }
+		return null;
+    }
+}
