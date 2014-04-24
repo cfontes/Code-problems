96f57516f02e19bd95ffa14d7b8a2d80c4728d5d
Cristiano Fontes <cristianofontes@gmail.com>
2014-04-24 10:09:53 +0800

    Path Sum 2 - not working yet

--- src/ojleet/problems/PathSum2.java
+++ src/ojleet/problems/PathSum2.java
@@ -1,5 +1,6 @@
 package ojleet.problems;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 import ojleet.helpers.BinaryTree;
@@ -14,32 +15,48 @@ public class PathSum2 {
 	public PathSum2() {
 		BinaryTree tree = new BinaryTree();
 		TreeNode root = tree.createOJTree(Arrays.asList("1", "-2", "-3", "1", "3", "-2", "#", "-1"));
-		System.out.println(hasPathSum(root, -1));
+		System.out.println(pathSum(root, -1));
 	}
 
-	public boolean hasPathSum(TreeNode root, int sum) {
+	public ArrayList<ArrayList<Integer>> pathSum(TreeNode root, int sum) {
 		if (root != null) {
-			return this.iterate(root, sum, root.val);
-		} else {
-			return false;
+			ArrayList<Integer> result = findSums(root, sum, root.val);
+			
+			
+			Fala fazer o recursivo dar mais de uma solucao
+			
+			
+			return new ArrayList<ArrayList<Integer>>();
 		}
+		return new ArrayList<ArrayList<Integer>>();
 	}
 
-	public boolean iterate(TreeNode root, int sum, int total) {
-		if (root.left == null && root.right == null && total == sum) {
-			return true;
+	public ArrayList<Integer> findSums(TreeNode root, int sum, int total) {				
+		if(root.left == null && root.right == null && total == sum){
+			ArrayList<Integer> path = new ArrayList<Integer>();
+			path.add(root.val);
+			return path;
+		}else if(root.left == null && root.right == null && total != sum){
+			return null;
 		}
 
-		if (root.left != null) {
-			if (this.iterate(root.left, sum, total + root.left.val))
-				return true;
+		if(root.left != null){
+			ArrayList<Integer> left = findSums(root.left, sum, total + root.left.val);
+			if(left != null){
+				left.add(root.left.val);
+				return left;
+			}
 		}
 
-		if (root.right != null) {
-			if (this.iterate(root.right, sum, total + root.right.val))
-				return true;
+		if(root.right != null){
+			ArrayList<Integer> right = findSums(root.right, sum, total + root.right.val);
+			if(right != null){
+				right.add(root.right.val);
+				return right;
+			}
 		}
-		return false;
+
+		return null;
 	}
 
 }
